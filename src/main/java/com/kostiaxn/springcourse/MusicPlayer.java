19a5e1c122d32115c73336743da0b76594b92548
kosti <kostia.xn@gmail.com>
2023-05-08 12:06:20 +0300

    Внедрение зависимостей. Dependency Injection (DI). Внедрение list в спринг

--- src/main/java/com/kostiaxn/springcourse/MusicPlayer.java
+++ src/main/java/com/kostiaxn/springcourse/MusicPlayer.java
@@ -1,17 +1,37 @@
 package com.kostiaxn.springcourse;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    // IoC
-    public MusicPlayer(Music music){
-        this.music = music;
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
     }
-    public MusicPlayer(){}
-    public void setMusic(Music music){
-        this.music = music;
+
+    public void setName(String name) {
+        this.name = name;
     }
-    public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public void playMusicList() {
+        for(Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
--- src/main/java/com/kostiaxn/springcourse/TestSpring.java
+++ src/main/java/com/kostiaxn/springcourse/TestSpring.java
@@ -8,11 +8,9 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-//        Music music = context.getBean("musicBean", Music.class);
-//
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+
+        musicPlayer.playMusicList();
 
         context.close();
     }
