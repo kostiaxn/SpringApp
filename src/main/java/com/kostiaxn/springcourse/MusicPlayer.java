863306a7bd9f9df97ee5ea962bed7ff15b79a3ee
kosti <kostia.xn@gmail.com>
2023-05-08 12:09:18 +0300

    Bean scope (Область видимости бинов).

--- src/main/java/com/kostiaxn/springcourse/MusicPlayer.java
+++ src/main/java/com/kostiaxn/springcourse/MusicPlayer.java
@@ -1,10 +1,7 @@
 package com.kostiaxn.springcourse;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MusicPlayer {
-    private List<Music> musicList = new ArrayList<>();
+    private Music music;
 
     private String name;
     private int volume;
@@ -25,13 +22,18 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
+    // IoC
+    public MusicPlayer(Music music) {
+        this.music = music;
     }
 
-    public void playMusicList() {
-        for(Music music : musicList) {
-            System.out.println("Playing: " + music.getSong());
-        }
+    public MusicPlayer() {}
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public void playMusic() {
+        System.out.println("Playing: " + music.getSong());
     }
 }
--- src/main/java/com/kostiaxn/springcourse/TestSpring.java
+++ src/main/java/com/kostiaxn/springcourse/TestSpring.java
@@ -8,9 +8,23 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        musicPlayer.playMusicList();
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+
+        System.out.println(comparison);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+        //System.out.println(musicPlayer.getName());
+        //System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
